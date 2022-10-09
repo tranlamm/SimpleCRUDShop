f48ee4ac1a1088e9e62490da940e0eb931c2505d
tranlamm <tranlam021102eee@gmail.com>
2022-10-09 21:24:47 +0700

    add redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,32 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import rootSaga from './saga';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
+import rootSaga from './saga';
 import productReducer from '~/redux/reducers/productSlice';
 
 // disalbe thunk and add redux-saga middleware
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 
+// redux persist
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, productReducer);
+
 export const store = configureStore({
     reducer: {
-        products: productReducer,
+        products: persistedReducer,
     },
     middleware,
 });
+
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
